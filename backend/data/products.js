7385821ffbb3b02b8ffb268c3d0fbcf800f8a1c8
albertohuerta <albertohuerta22@gmail.com>
2022-08-07 16:03:24 +0100

    created backend folder, created model,  added data products, users

---
+++ backend/data/products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    name: 'Red Tamales',
+    image: '/images/airpods.jpg',
+    description: 'Red Tamale, contains marinated Pork',
+    price: 2.99,
+    rating: 4.5,
+  },
+  {
+    name: 'Green Tamales',
+    image: '/images/airpods.jpg',
+    description: 'Red Tamale, contains marinated Pork',
+    price: 2.99,
+    rating: 4.5,
+  },
+  {
+    name: 'Mole',
+    image: '/images/airpods.jpg',
+    description: 'Red Tamale, contains marinated Pork',
+    price: 2.99,
+    rating: 4.5,
+  },
+  {
+    name: 'Champurrado',
+    image: '/images/airpods.jpg',
+    description: 'Red Tamale, contains marinated Pork',
+    price: 2.99,
+    rating: 4.5,
+  },
+];
+
+export default products;
