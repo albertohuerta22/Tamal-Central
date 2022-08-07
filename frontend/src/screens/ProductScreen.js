23088beec22a1ef196accb989d0cb947cf0994b3
albertohuerta <albertohuerta22@gmail.com>
2022-08-07 16:13:12 +0100

    created images folder, created productscreen, added images

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+  Form,
+} from 'react-bootstrap';
+
+const ProductScreen = () => {
+  return <div>ProductScreen</div>;
+};
+
+export default ProductScreen;
