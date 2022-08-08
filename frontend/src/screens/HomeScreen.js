2a9fd7cb268d2d5ccf720086c2d5c58ae9ab96be
albertohuerta <albertohuerta22@gmail.com>
2022-08-08 02:17:13 +0100

    created loader, message, proudct components, added to homescreen, products not currenly being rendered

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
-import { listProducts } from '../action/productActions';
+// import { listProducts } from '../action/productActions';
+
+//components imported
+import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const HomeScreen = () => {
-  return <Row></Row>;
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  return (
+    <>
+      <h1>Latest Dishes</h1>
+      {loading ? (
+        <Loader></Loader>
+      ) : error ? (
+        <Message variant="danger"></Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
 };
 
 export default HomeScreen;
