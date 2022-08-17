7761e3319c9f6bd0178c85d5139c762ac4b999eb
albertohuerta <albertohuerta22@gmail.com>
2022-08-17 20:54:04 +0100

    created welcome screen, working on couresel

--- frontend/src/screens/WelcomeScreen.js
+++ frontend/src/screens/WelcomeScreen.js
@@ -2,13 +2,18 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import { listProducts } from '../action/productActions';
+import Carousel from 'react-bootstrap/Carousel';
 
 //components imported
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import redTamale from '../images/redtamale.jpg';
+import greenTamale from '../images/greentamale.jpg';
+import mole from '../images/mole.jpg';
+import champurrado from '../images/champurrado.jpg';
 
-const HomeScreen = () => {
+const WelcomeScreen = () => {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
@@ -19,30 +24,48 @@ const HomeScreen = () => {
   }, [dispatch]);
 
   return (
-    <>
-      <h1>Latest Dishes</h1>
-      {loading ? (
-        <Loader></Loader>
-      ) : error ? (
-        <Message variant="danger"></Message>
-      ) : (
-        <Row>
-          {products.map((product) => (
-            <Col
-              key={product._id}
-              sm={12}
-              md={6}
-              lg={4}
-              xl={3}
-              // className="align-items-stretch d-flex"
-            >
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
-      )}
-    </>
+    <div className="main-container">
+      <Carousel variant="dark">
+        <Carousel.Item
+          style={{
+            height: '32rem',
+            background: '#777',
+            color: 'white',
+            position: 'relative',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}
+        >
+          <img className="h-50" src={redTamale} alt="red tamale" />
+          <Carousel.Caption>
+            <h5>Red Tamale</h5>
+            <p>Food Description Here</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="w-50 mh-50" src={greenTamale} alt="green tamale" />
+          <Carousel.Caption>
+            <h5>Green Tamale</h5>
+            <p>Food Description Here.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="w-50 mh-50" src={mole} alt="mole" />
+          <Carousel.Caption>
+            <h5>Mole</h5>
+            <p>Food Description Here</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="w-50 h-20" src={champurrado} alt="champurrado" />
+          <Carousel.Caption>
+            <h5>Champurrado</h5>
+            <p>Food Description Here</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 };
 
-export default HomeScreen;
+export default WelcomeScreen;
